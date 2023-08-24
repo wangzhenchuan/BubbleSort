31051e9b18a68ef5898398bab9b0e3991be13e83
wangzhenchuan <191623431@qq.com>
2023-08-24 15:01:20 +0800

    add bubble sort code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BubbleSort
 使用Rust语言写一个冒泡排序的算法
+基础要求：固定类型（比如i32）的数组排序
+提高部分：能够使用template和PartialOrd实现对任意类型的排序
+![Alt text](image.png)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn bubble_sort<T: PartialOrd>(arr: &mut [T]) {
+    let n = arr.len();
+    for i in 0..n {
+        for j in 0..n - i - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut arr = [4, 2, 9, 5, 1];
+    bubble_sort(&mut arr);
+    println!("{:?}", arr);
+
+    let mut arr2 = ["banana", "apple", "orange", "pineapple"];
+    bubble_sort(&mut arr2);
+    println!("{:?}", arr2);
 }
